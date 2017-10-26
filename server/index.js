bdc9a406119a3d2be4f9926219cd7cab7a113cf0
Andrew Rossman <andrew100r@gmail.com>
2017-10-26 15:17:45 -0700

    (refactor) Use promises for selecting all user events

--- server/index.js
+++ server/index.js
@@ -7,33 +7,59 @@ const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  db.selectAllEvents((err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
+  // db.selectAllEvents((err, data) => {
+  //   if (err) {
+  //     res.sendStatus(500);
+  //   } else {
+  //     res.status(200).send(data);
+  //   }
+  // });
+  db.selectAllEvents()
+    .then((data) => {
       res.status(200).send(data);
-    }
-  });
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
 });
 
 app.get('/userEventData', (req, res) => {
-  generateEvents(20000);
-  res.sendStatus(200);
+  generateEvents(20000)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
 });
 
 app.get('/:session', (req, res) => {
-  db.selectSessionEvents(req.params.session, (err, data) => {
-    if (err) {
-      res.sendStatus(500);
-    } else {
+  // db.selectSessionEvents(req.params.session, (err, data) => {
+  //   if (err) {
+  //     res.sendStatus(500);
+  //   } else {
+  //     res.status(200).send(data);
+  //   }
+  // });
+  db.selectSessionEvents(req.params.session)
+    .then((data) => {
       res.status(200).send(data);
-    }
-  });
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
 });
 
 app.post('/', (req, res) => {
-  db.addEvent(req.body.session, req.body.query);
-  res.sendStatus(201);
+  // db.addEvent(req.body.session, req.body.query);
+  // res.sendStatus(201);
+  db.addEvent(req.body.session, req.body.query)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(() => {
+      res.sendStatus(500);
+    });
 });
 
 
